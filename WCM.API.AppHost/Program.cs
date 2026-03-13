5c9eadda2af9af1451c9a9b41e89370e6d096d67
Alvaro <212876443+aechegoyan@users.noreply.github.com>
2026-03-13 11:58:43 +0100

    chore(aspire): update Aspire setup
    add compose environment
    add k8s environment

--- WCM.API.AppHost/Program.cs
+++ WCM.API.AppHost/Program.cs
@@ -1,14 +1,42 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var compose = builder.AddDockerComposeEnvironment("compose")
+    .WithDashboard(dashboard =>
+    {
+        dashboard.WithHostPort(8080)
+                .WithForwardedHeaders(enabled: true);
+    });
+
+var k8s = builder.AddKubernetesEnvironment("k8s")
+    .WithProperties(k8s =>
+    {
+        k8s.HelmChartName = "wcm-api";
+    });
+
 var postgres = builder.AddPostgres("postgres")
+    .WithLifetime(ContainerLifetime.Persistent)
     .WithDataVolume("wcm-postgres-data")
-    .WithPgAdmin();
+    .WithPgAdmin()
+    .WithComputeEnvironment(compose);
 
 var wcmDatabase = postgres.AddDatabase("wcmdb");
 
 var apiService = builder.AddProject<Projects.WCM_API_ApiService>("apiservice")
     .WithReference(wcmDatabase)
     .WaitFor(wcmDatabase)
-    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "LocalDevelopment");
+    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "LocalDevelopment")
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "api";
+    })
+    .WithComputeEnvironment(compose);
+
+//  temporary setup adding custom Container image
+// var container = builder.ExecutionContext.IsRunMode
+//     ? builder.AddDockerfile(
+//           "wcm-api", "WCM.API.ApiService", "Containerfile.debug")
+//     : builder.AddDockerfile(
+//           "wcm-api", "WCM.API.ApiService", "Containerfile.release")
+//     .WithComputeEnvironment(compose);
 
 builder.Build().Run();
